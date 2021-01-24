9ca15cf55efa8774f5a3ce188f32be2da8f9ae34
Chad Oakenfold <web@oakenfold.ca>
2021-01-24 12:54:12 -0800

    Changed csv parsing (header = true)

--- src/service/csv.js
+++ src/service/csv.js
@@ -1,6 +1,8 @@
 import Papa from "papaparse";
 export const parse = (csv) => {
-  return Papa.parse(csv, {
+  const parsed = Papa.parse(csv, {
     dynamicTyping: true,
+    header: true,
   });
+  return parsed;
 };
